ae2b521551d6339723078b43eee10c8d6e48e509
wanping <wanping@58.com>
2020-11-02 19:47:57 +0800

    添加大于对象大小

--- src/main/java/test/Test1.java
+++ src/main/java/test/Test1.java
@@ -1,11 +1,13 @@
 package test;
 
+import java.util.HashMap;
+
 public class Test1 {
 
-  public static void main(String[] args) {
-    int x = 23897;
-    System.out.println(x %= 10);
-    int y = 23897;
-    System.out.println(y /= 10);
-  }
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+        System.out.println(test1.hashCode());
+        System.out.println(System.identityHashCode(test1));
+        System.out.println(System.identityHashCode(test1));
+    }
 }
